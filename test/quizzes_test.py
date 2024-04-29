f0d17907f816870bcc67ade11198ba11f3daac5d
Manasi013 <90250760+Manasi013@users.noreply.github.com>
2024-04-29 19:55:03 -0400

    Revert "Team 4_Deliverable E"

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -13,38 +13,7 @@ class QuizzesTest(unittest.TestCase):
         Implement this function and two more that
         execute the code and make it fail.
         """
-        self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz(None, "Quiz", "start date ","due date")
-        quiz = self.ctrl.get_quizzes()
-        self.assertEquals(len(quiz), 1, "There is exactly one discussion.")
-        quiz = self.ctrl.get_quiz_by_id(quiz_id)
-        self.ctrl.print_quiz(quiz_id)
-        self.assertIsNotNone(quiz, "The quiz can be retrieved.")
-
-    def test_expose_failure_02(self):
-        """
-        Implement this function and two more that
-        execute the code and make it fail.
-        """
-        self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz("Quiz title", "Quiz", "start date ","due date")
-        question_id = self.ctrl.add_question(quiz_id,"Hello"+5,"Question Title")
-        question = self.ctrl.get_question_by_id(question_id)
-        self.ctrl.print_quiz(quiz_id)
-        self.assertIsNotNone(question, "The quiz can be retrieved.")
-
-    def test_expose_failure_03(self):
-        """
-        Implement this function and two more that
-        execute the code and make it fail.
-        """
-        self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz("Quiz title", "Quiz", "start date ","due date")
-        question_id = self.ctrl.add_question(quiz_id,"Question Title","Question")
-        Answer_id = self.ctrl.add_answer(question_id,"3"+5,True)
-        Answer = self.ctrl.print_quiz(quiz_id)
-        self.assertIsNone(Answer, "The quiz can be retrieved.")
-        
+        self.assertTrue(True, 'Example assertion.')
         
 
 if __name__ == '__main__':
